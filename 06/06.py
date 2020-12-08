3a436e7d326c863197256ef6132048a421d959d6
TomDBR <canardo@cock.li>
2020-12-08 21:28:44 +0100

    DAY08, small day6 comment so I remember how to use that

--- 06/06.py
+++ 06/06.py
@@ -5,7 +5,7 @@ scores = [0,0]
 for group in data:
     g_data = {}
     for ch in ''.join(group): # might as well concat the whole thing for a group together
-        g_data[ch] = g_data.setdefault(ch, 0) + 1
+        g_data[ch] = g_data.setdefault(ch, 0) + 1 # returns g_data[ch] if it's present, else 0
     scores[0] += len(g_data)
     scores[1] += len(dict(filter(lambda g: g[1] == len(group), g_data.items())))
 
